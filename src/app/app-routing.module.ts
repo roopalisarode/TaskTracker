0a006f088b7adaf9a664fedad1f73d98ab8afe10
Roopali Sarode <114116643+roopalisarode@users.noreply.github.com>
2023-08-17 16:32:17 -0400

    Frontend initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
+import { DeletedTodoComponent } from './components/deleted-todo/deleted-todo.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent
+
+  },
+  {
+    path: 'todos',
+    component: TodosComponent
+  },
+  {
+    path: 'deleted-todo',
+    component: DeletedTodoComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
