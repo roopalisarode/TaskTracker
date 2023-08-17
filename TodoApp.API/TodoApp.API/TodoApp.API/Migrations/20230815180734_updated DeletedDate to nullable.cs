f9e479bf4de68b606d6e0341e5260c0b3fd5ac61
Roopali Sarode <114116643+roopalisarode@users.noreply.github.com>
2023-08-17 16:35:21 -0400

    Backend inital commit

---
+++ TodoApp.API/TodoApp.API/TodoApp.API/Migrations/20230815180734_updated DeletedDate to nullable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedDeletedDatetonullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeltedDate",
+                table: "Todos");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Todos",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Todos");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeltedDate",
+                table: "Todos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
