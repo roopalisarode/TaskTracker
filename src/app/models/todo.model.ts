24f789cb47344cff6a9375479536a2f6f9c7191a
Roopali Sarode <114116643+roopalisarode@users.noreply.github.com>
2023-08-17 16:39:52 -0400

    Delete src directory

--- src/app/models/todo.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Todo {
-    id: string,
-    description: string,
-    createdDate: Date,
-    isCompleted: boolean,
-    completedDate: Date,
-    isDeleted: boolean,
-    deletedDate: Date
-};
